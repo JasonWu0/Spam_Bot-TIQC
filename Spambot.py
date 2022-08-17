542b38caeaad0f983f9fdf430a862b24c8cb5c07
JasonWu0 <94402761+JasonWu0@users.noreply.github.com>
2022-08-17 16:48:30 -0400

    Update Spambot.py

--- Spambot.py
+++ Spambot.py
@@ -3,19 +3,22 @@ import time
 import pyautogui
 
 def SendHelp():
+    #Time before bot starts typing
     time.sleep(5)
-
+    #Which text do you want to copy and type from
     text = open("HELP ME.txt")
     for each_line in text:
         pyautogui.typewrite(each_line)
         pyautogui.press("enter")
 
 def SendWake():
+    #Which text do you want to copy and type from
     text = open("WAKE UP.txt")
     for each_line in text:
         pyautogui.typewrite(each_line)
         pyautogui.press("enter")
 
+#Confirm you want to start bot
 username = input("Do you want to start spam bot?: ")
 if username.upper() == "Y" or username.upper() == "YES":
     time.sleep(5)
@@ -23,5 +26,5 @@ if username.upper() == "Y" or username.upper() == "YES":
         SendWake()
         pyautogui.press("enter")
 
-
+#If you want to stop bot
 pyautogui.FAILSAFE = True
